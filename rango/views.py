8da8ade296d3388fa7642cc5c279b046ef0ffdc7
Parsa Tavakoli <2555801t@student.gla.ac.uk>
2022-01-21 11:13:56 +0000

    3.5

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("rango says hey there partner!")
+    
+# Create your views here.
